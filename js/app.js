ecc4cd5783e2c4745b78752d1a9f3d349bc22ff6
Diana Kim <dianajkim@gmail.com>
2019-10-27 17:33:18 -0700

    image template renders pictures

--- js/app.js
+++ js/app.js
@@ -11,21 +11,43 @@ function Image(image) {
 Image.allImages = [];
 let allKeywords = [];
 
-Image.prototype.render = function() {
-  $('main').append('<div class="clone"></div>');
-  let imageClone = $('div[class="clone"]');
-  let imageHtml = $('#photo-template').html();
+Image.prototype.toHtml = function() {
+  let template = $('#photo-template').html();
+  let templateRender = Handlebars.compile(template);
+  return templateRender(this);
+};
 
-  allKeywords.push(this.keyword);
+Image.getJsonData = (filePath) => {
+  $.get(filePath, 'json')
+    .then(data => {
+      data.forEach(item => {
+        Image.allImages.push(new Image(item));
+      });
+    })
+    .then(Image.loadImages);
+};
 
-  imageClone.html(imageHtml);
-  imageClone.find('h2').text(this.title);
-  imageClone.find('img').attr('src', this.image_url);
-  imageClone.find('p').text(this.description);
-  imageClone.attr('data-keyword', this.keyword);
-  imageClone.removeClass('clone');
-  imageClone.attr('class', this.title);
-  $('#photo-template').hide();
+Image.loadImages = () => {
+  Image.allImages.forEach(imageToRender => {
+    $('#photos-container').append(imageToRender.toHtml());
+  });
+};
+
+Image.loadFilterList = () => {
+  // first, remove duplicate values in allKeywords array
+  const uniqueKeywords = Array.from(new Set(allKeywords));
+  let keyList = $('select[id="keyword-dropdown"]');
+  // empty dropdown list
+  keyList.find('option').not(':first').remove();
+  
+  // for each item in uniqueKeywords, add a new dropdown list item
+  $.each(uniqueKeywords, function(index, value){
+    $('select').append('<option class="keyword-clone"></option>');
+    let keywordOptionClone = $('option[class="keyword-clone"]');
+    keywordOptionClone.text(value);
+    keywordOptionClone.removeClass('keyword-clone');
+    keywordOptionClone.attr('class', value);
+  });
 };
 
 $('select[name="keyword"]').on('change', function() {
@@ -51,42 +73,8 @@ $('.page').on('click', function() {
   Image.getJsonData(jsonFilePath);
 });
 
-function loadFilterList(){
-  // first, remove duplicate values in allKeywords array
-  const uniqueKeywords = Array.from(new Set(allKeywords));
-  let keyList = $('select[id="keyword-dropdown"]');
-  // empty dropdown list
-  keyList.find('option').not(':first').remove();
-
-  // for each item in uniqueKeywords, add a new dropdown list item
-  $.each(uniqueKeywords, function(index, value){
-    $('select').append('<option class="keyword-clone"></option>');
-    let keywordOptionClone = $('option[class="keyword-clone"]');
-    keywordOptionClone.text(value);
-    keywordOptionClone.removeClass('keyword-clone');
-    keywordOptionClone.attr('class', value);
-  });
-}
-
-Image.getJsonData = (filePath) => {
-  $.get(filePath, 'json')
-    .then(data => {
-      data.forEach(item => {
-        Image.allImages.push(new Image(item));
-      });
-    })
-    .then(Image.loadImages);
-};
-
-Image.loadImages = () => {
-  Image.allImages.forEach(image => image.render());
-  loadFilterList();
-};
-
 $(() => {
   Image.getJsonData('data/page-1.json');
+  Image.loadImages();
+  Image.loadFilterList();
 });
-
-// $(document).ready( () => {
-//   Image.getJsonData();
-// });
